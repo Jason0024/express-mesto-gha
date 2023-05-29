b5b5053de6edb90b5ce52db3f0524c4d1d226d40
Sergey Shamardin <s.shamrdin@gmail.com>
2023-05-29 18:17:03 +0700

    fix/ next preset (2)

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,23 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../utils/response-errors/Unauthorized');
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
-
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new Unauthorized('Необходима авторизация'));
+    return next(new Unauthorized('Для доступа необходимо выполнить авторизацию'));
   }
 
-  const token = extractBearerToken(authorization);
   let payload;
+  const userToken = authorization.replace('Bearer ', '');
 
   try {
-    payload = jwt.verify(token, 'token-generate-key');
-  } catch (err) {
-    next(new Unauthorized('Необходима авторизация'));
+    payload = jwt.verify(userToken, 'token-generate-key');
+  } catch (_) {
+    return next(new Unauthorized('Для доступа необходимо выполнить авторизацию'));
   }
 
   req.user = payload;
-
   next();
 };
